023194ecda399302129701965c22f70e78dfe4d4
linabiel <linuka.b@gmail.com>
2021-05-28 12:44:33 +0100

    tests fixed

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -1,18 +1,24 @@
+from typing import Sequence
 import unittest
 from src.card import Card
 from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
+
     def setUp(self):
-        self.card1 = Card("ace", 5)
+        self.card1 = Card("ace", 1)
         self.card2 = Card("clubs", 9)
 
+        self.cards = [self.card1, self.card2]
+
     def test_can_check_for_ace(self):
-        self.assertEqual('ace', self.card1.suit)
+        check_if_is_ace = CardGame.check_for_ace(self, self.card1)
+        self.assertEqual(True, check_if_is_ace)
 
     def test_highest_card(self):
-        self.assertEqual(self.card2.value, self.card2.value)
+        check_highest_card = CardGame.highest_card(self, self.card1, self.card2)
+        self.assertEqual(self.card2.value, check_highest_card.value)
 
     def test_cards_total(self):
-        cards = self.card1.value + self.card2.value
-        self.assertEqual(14, cards)
+        cards_total = CardGame.cards_total(self, self.cards)
+        self.assertEqual("You have a total of 10", cards_total)
